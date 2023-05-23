7772bffec3cbd9c7ce622a4cb3a32fee3ca43ba8
Tristanslayer <tristansibug@gmail.com>
2023-05-24 02:20:31 +0800

    fixed the about section, yawqna

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,14 @@
 import './About.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AiFillCheckCircle } from 'react-icons/ai';
 import { FaFacebook } from 'react-icons/fa';
 import { BsTwitter } from 'react-icons/bs';
 import { BsInstagram } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
-import React, { useState } from 'react';
-import { IoIosArrowDroprightCircle, IoIosArrowDropleftCircle } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
 
 function About() {
   return (
     <div>
-      <div id="footer" className="container-fluid footer2">
+      <div className="container-fluid footer2">
         <div className="row footer-firstelem">
           <div className="col-5 offset-1">
             <h2 className="technological display-2">Technological Integration In Clinical Assessments</h2>
@@ -43,12 +39,45 @@ function About() {
               <p>Address: 123 Quirino Avenue, Davao City, Philippines</p>
               <p className="contact-info-lastline">Feel free to reach out to us with any questions or concerns you may have. We look forward to hearing from you!</p>
             </div>
+
+            <div className="row mt-5">
+              <div className="col-12 text-start">
+                <h3 className="socials">Socials</h3>
+              </div>
+              <div className="col-12 col-sm-4 mt-3">
+                <div className="socials-info">
+                  <div className="row">
+                    <div className="col-4 col-md-6 col-sm-5 social-logos"><FaFacebook />
+                    </div>
+                    <div className="col-8 col-md-6 col-sm-7 align-self-center socialtext">facebook.com/Labtica
+                    </div>
+
+
+                    <div className="col-4 col-md-6 col-sm-5 social-logos"><BsTwitter />
+                    </div>
+                    <div className="col-8 col-md-6 col-sm-7 align-self-center socialtext">twitter.com/Labtica
+                    </div>
+
+                    <div className="col-4 col-md-6 col-sm-5 social-logos"><BsInstagram />
+                    </div>
+                    <div className="col-8 col-md-6 col-sm-7 align-self-center socialtext">instagram.com/Labtica
+                    </div>
+
+                    <div className="col-4 col-md-6 col-sm-5 social-logos"><BsLinkedin />
+                    </div>
+                    <div className="col-8 col-md-6 col-sm-7 align-self-center socialtext">linkedin.com/company/Labtica
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-1"></div>
         </div>
 
-        <div className="row mt-5">
-          {/* <div className="col-3 me-auto">
+        {/*<div className="row mt-5">
+           <div className="col-3 me-auto">
             <div className="row">
               <div className="col-12">
                 <h3 className="language">Language</h3>
@@ -62,46 +91,8 @@ function About() {
                 </select>
               </div>
             </div>
-          </div> */}
-
-          <div className="col">
-            <div className="row">
-              <div className="col-12">
-                <h3 className="socials">Socials</h3>
-              </div>
-              <div className="col-4 offset-3 offset-sm-4 offset-md-5 offset-lg-6">
-                <div className="socials-info">
-                  <div className="row">
-                    <div className="col-4 col-md-3 col-sm-2 social-logos"><FaFacebook />
-                    </div>
-                    <div className="col-6 col-md-7 col-sm-8 align-self-center socialtext">facebook.com/Labtica
-                    </div>
-                    <div className="col-2"></div>
-
-
-                    <div className="col-4 col-md-3 col-sm-2 social-logos"><BsTwitter />
-                    </div>
-                    <div className="col-5 col-md-6 col-sm-7 align-self-center socialtext">twitter.com/Labtica
-                    </div>
-                    <div className="col-3"></div>
-
-                    <div className="col-4 col-md-3 col-sm-2 social-logos"><BsInstagram />
-                    </div>
-                    <div className="col-6 col-md-7 col-sm-8 align-self-center socialtext">instagram.com/Labtica
-                    </div>
-                    <div className="col-2"></div>
-
-                    <div className="col-4 col-md-3 col-sm-2 social-logos"><BsLinkedin />
-                    </div>
-                    <div className="col-8 col-md-7 col-sm-8 align-self-center socialtext">linkedin.com/company/Labtica
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
+          </div>*/}
       </div>
       <div className="container">
         <p className="footer3-info text-center my-5">© 2023 CBI Group Company. All right reserved.</p>
